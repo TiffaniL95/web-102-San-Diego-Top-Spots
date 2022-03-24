d27e915967c438879a356fc3bd9ffc01981f6fcf
Tiffani <TiffaniL95@gmail.com>
2022-03-24 11:03:31 -0700

    html and basic js completed

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
+// const { append } = require("express/lib/response");
+
 $(document).ready(function() {
 // write your code here
+$('tbody').html('');
+$.getJSON('data.json', function(data){
+    $.each(data, function(i, data){
 
+        $(`
+        <tr> 
+        <td>${data.name}</td>
+        <td>${data.description}</td>
+        <td><a href='https://www.google.com/maps?q=${data.location}'><button>Open in Google Maps</button></a></td>
+        </tr>`).appendTo('tbody');
+    })
+})
 });
+
+
